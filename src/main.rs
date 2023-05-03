4fc54966791b657608a8c680dbe232061d24d6fc
EXSPIRAVIT_1104 <m18621006730@gmail.com>
2023-05-03 16:57:19 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,11 @@
 mod styles;
 
-use styles::*;
 use bevy::{
     prelude::*,
-    window::{
-        PresentMode, 
-        WindowPlugin,
-    },
+    window::{PresentMode, WindowPlugin},
     winit::WinitSettings,
 };
-use std::time::Duration;
+use styles::*;
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Default, States)]
 pub enum AppState {
@@ -28,92 +24,100 @@ struct Background;
 struct Grid;
 
 #[derive(Component)]
-struct Splash;
+struct MainText;
 
 fn main() {
     App::new()
         .insert_resource(Msaa::Sample4)
         .insert_resource(ClearColor(Color::hex("#3A3A3C").unwrap()))
-        .insert_resource(WinitSettings::desktop_app())        
-        .add_plugins(DefaultPlugins
-            .set(WindowPlugin {
-                primary_window: Some(Window {
-                    title: "TUNE MILL™".into(),
-                    resolution: (480., 480.).into(),
-                    fit_canvas_to_parent: true,
-                    present_mode: PresentMode::AutoNoVsync,
+        .insert_resource(WinitSettings::desktop_app())
+        .add_plugins(
+            DefaultPlugins
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: "TUNE MILL™".into(),
+                        resolution: (480., 480.).into(),
+                        fit_canvas_to_parent: true,
+                        present_mode: PresentMode::AutoNoVsync,
+                        ..default()
+                    }),
+                    ..default()
+                })
+                .set(AssetPlugin {
+                    watch_for_changes: true,
                     ..default()
                 }),
-                ..default()
-            })
-            .set(AssetPlugin {
-                watch_for_changes: true,
-                ..default()
-            })
         )
         .add_state::<AppState>()
         .add_startup_system(setup)
         .run();
 }
 
-fn setup(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-) {
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2dBundle::default());
-    
-    commands.spawn((
-        TextBundle::from_sections([
-            TextSection::new(
-                "TUNE MILL",
-                TextStyle {
-                    font: asset_server.load("fonts/PPMori-Regular.otf"),
-                    font_size: 42.0,
-                    color: color(WHITE),
-                },
-            ),
-            TextSection::new(
-                "™",
-                TextStyle {
-                    font: asset_server.load("fonts/PPMori-Regular.otf"),
-                    font_size: 42.0,
-                    color: color(LIGHT_GREY),
-                },
-            ),
-        ])
-        .with_text_alignment(TextAlignment::Center)
-        .with_style(Style {
-            position_type: PositionType::Absolute,
-            size: Size::new(Val::Px(480.0), Val::Px(480.0)),
-            justify_content: JustifyContent::Center,
-            align_items: AlignItems::Center,
-            ..default()
-        }),
-        Splash,
-    ));
 
-    commands.spawn(SpriteBundle {
-        sprite: Sprite {
-            // Alpha channel of the color controls transparency.
-            color: Color::rgba(1.0, 1.0, 1.0, 1.0),
+    commands
+        .spawn(NodeBundle {
+            style: MAIN_TEXT_STYLE,
             ..default()
-        },
-        texture: asset_server.load("grid.png"),
-        transform: Transform::from_xyz(0.0, 0.0, 2.0),
-        ..default()
-    })
-    .insert(Grid);
+        })
+        .with_children(|parent| {
+            parent.spawn(
+                TextBundle::from_section(
+                    "TUNE MILL",
+                    TextStyle {
+                        font: asset_server.load("fonts/PPMori-Regular.otf"),
+                        font_size: 40.,
+                        color: color(WHITE),
+                    },
+                )
+                .with_text_alignment(TextAlignment::Center)
+                .with_style(Style {
+                    position: UiRect {
+                        bottom: Val::Px(2.5),
+                        ..default()
+                    },
+                    ..default()
+                }),
+            );
 
-    commands.spawn(SpriteBundle {
-        sprite: Sprite {
-            // Alpha channel of the color controls transparency.
-            color: Color::rgba(1.0, 1.0, 1.0, 1.0),
+            parent.spawn(
+                TextBundle::from_section(
+                    "TM",
+                    TextStyle {
+                        font: asset_server.load("fonts/PPMori-Extralight.otf"),
+                        font_size: 20.,
+                        color: color(LIGHT_GREY),
+                    },
+                )
+                .with_text_alignment(TextAlignment::Center),
+            );
+        })
+        .insert(MainText);
+
+    commands
+        .spawn(SpriteBundle {
+            sprite: Sprite {
+                // Alpha channel of the color controls transparency.
+                color: Color::rgba(1., 1., 1., 1.),
+                ..default()
+            },
+            texture: asset_server.load("grid.png"),
+            transform: Transform::from_xyz(0., 0., 2.),
             ..default()
-        },
-        texture: asset_server.load("background.png"),
-        transform: Transform::from_xyz(0.0, 0.0, 1.0),
-        ..default()
-    })
-    .insert(Background);
+        })
+        .insert(Grid);
+
+    commands
+        .spawn(SpriteBundle {
+            sprite: Sprite {
+                // Alpha channel of the color controls transparency.
+                color: Color::rgba(1., 1., 1., 1.),
+                ..default()
+            },
+            texture: asset_server.load("background.png"),
+            transform: Transform::from_xyz(0., 0., 1.),
+            ..default()
+        })
+        .insert(Background);
 }
-
--- src/styles.rs
+++ src/styles.rs
@@ -1,5 +1,21 @@
 use bevy::prelude::*;
 
+pub const MAIN_TEXT_STYLE: Style = Style {
+    display: Display::Flex,
+    flex_direction: FlexDirection::Row,
+    justify_content: JustifyContent::Center,
+    align_items: AlignItems::FlexStart,
+    position_type: PositionType::Absolute,
+    margin: UiRect { 
+        left: Val::Auto,
+        right: Val::Auto, 
+        top: Val::Auto, 
+        bottom: Val::Auto,
+    },
+    size: Size::new(Val::Percent(100.), Val::Px(30.)),
+    ..Style::DEFAULT
+};
+
 pub const WHITE : &str = "#F2F2F7";
 pub const LIGHT_GREY : &str = "#8E8E93";
 
